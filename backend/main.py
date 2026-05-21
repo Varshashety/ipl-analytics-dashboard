beb05d90d858e73868911beff60680d6ed72cee4
mithinti-varshini <mithinti@shnoor.com>
2026-05-21 20:24:03 +0530

    Updated CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -14,11 +14,13 @@ Base.metadata.create_all(bind=engine)
 # FastAPI app
 app = FastAPI()
 
+# CORS Configuration
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://ipl-analytics-dashboard-ebon.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
